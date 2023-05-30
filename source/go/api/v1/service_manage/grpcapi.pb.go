5fbfacf124e087cf5efd1f461aecc9622a75bd5d
chuntaojun <liaochuntao@live.com>
2023-05-30 11:13:13 +0800

    refactor:调整heartbeat新增接口的归属Service

--- source/go/api/v1/service_manage/grpcapi.pb.go
+++ source/go/api/v1/service_manage/grpcapi.pb.go
@@ -45,12 +45,6 @@ type PolarisGRPCClient interface {
 	Discover(ctx context.Context, opts ...grpc.CallOption) (PolarisGRPC_DiscoverClient, error)
 	// 被调方上报心跳
 	Heartbeat(ctx context.Context, in *Instance, opts ...grpc.CallOption) (*Response, error)
-	// 被调方批量上报心跳
-	BatchHeartbeat(ctx context.Context, opts ...grpc.CallOption) (PolarisGRPC_BatchHeartbeatClient, error)
-	// 批量获取心跳记录
-	BatchGetHeartbeat(ctx context.Context, in *GetHeartbeatsRequest, opts ...grpc.CallOption) (*GetHeartbeatsResponse, error)
-	// 批量删除心跳记录
-	BatchDelHeartbeat(ctx context.Context, in *DelHeartbeatsRequest, opts ...grpc.CallOption) (*DelHeartbeatsResponse, error)
 }
 
 type polarisGRPCClient struct {
@@ -128,55 +122,6 @@ func (c *polarisGRPCClient) Heartbeat(ctx context.Context, in *Instance, opts ..
 	return out, nil
 }
 
-func (c *polarisGRPCClient) BatchHeartbeat(ctx context.Context, opts ...grpc.CallOption) (PolarisGRPC_BatchHeartbeatClient, error) {
-	stream, err := c.cc.NewStream(ctx, &_PolarisGRPC_serviceDesc.Streams[1], "/v1.PolarisGRPC/BatchHeartbeat", opts...)
-	if err != nil {
-		return nil, err
-	}
-	x := &polarisGRPCBatchHeartbeatClient{stream}
-	return x, nil
-}
-
-type PolarisGRPC_BatchHeartbeatClient interface {
-	Send(*HeartbeatsRequest) error
-	Recv() (*HeartbeatsResponse, error)
-	grpc.ClientStream
-}
-
-type polarisGRPCBatchHeartbeatClient struct {
-	grpc.ClientStream
-}
-
-func (x *polarisGRPCBatchHeartbeatClient) Send(m *HeartbeatsRequest) error {
-	return x.ClientStream.SendMsg(m)
-}
-
-func (x *polarisGRPCBatchHeartbeatClient) Recv() (*HeartbeatsResponse, error) {
-	m := new(HeartbeatsResponse)
-	if err := x.ClientStream.RecvMsg(m); err != nil {
-		return nil, err
-	}
-	return m, nil
-}
-
-func (c *polarisGRPCClient) BatchGetHeartbeat(ctx context.Context, in *GetHeartbeatsRequest, opts ...grpc.CallOption) (*GetHeartbeatsResponse, error) {
-	out := new(GetHeartbeatsResponse)
-	err := c.cc.Invoke(ctx, "/v1.PolarisGRPC/BatchGetHeartbeat", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *polarisGRPCClient) BatchDelHeartbeat(ctx context.Context, in *DelHeartbeatsRequest, opts ...grpc.CallOption) (*DelHeartbeatsResponse, error) {
-	out := new(DelHeartbeatsResponse)
-	err := c.cc.Invoke(ctx, "/v1.PolarisGRPC/BatchDelHeartbeat", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
 // PolarisGRPCServer is the server API for PolarisGRPC service.
 type PolarisGRPCServer interface {
 	// 客户端上报
@@ -189,12 +134,6 @@ type PolarisGRPCServer interface {
 	Discover(PolarisGRPC_DiscoverServer) error
 	// 被调方上报心跳
 	Heartbeat(context.Context, *Instance) (*Response, error)
-	// 被调方批量上报心跳
-	BatchHeartbeat(PolarisGRPC_BatchHeartbeatServer) error
-	// 批量获取心跳记录
-	BatchGetHeartbeat(context.Context, *GetHeartbeatsRequest) (*GetHeartbeatsResponse, error)
-	// 批量删除心跳记录
-	BatchDelHeartbeat(context.Context, *DelHeartbeatsRequest) (*DelHeartbeatsResponse, error)
 }
 
 func RegisterPolarisGRPCServer(s *grpc.Server, srv PolarisGRPCServer) {
@@ -299,68 +238,6 @@ func _PolarisGRPC_Heartbeat_Handler(srv interface{}, ctx context.Context, dec fu
 	return interceptor(ctx, in, info, handler)
 }
 
-func _PolarisGRPC_BatchHeartbeat_Handler(srv interface{}, stream grpc.ServerStream) error {
-	return srv.(PolarisGRPCServer).BatchHeartbeat(&polarisGRPCBatchHeartbeatServer{stream})
-}
-
-type PolarisGRPC_BatchHeartbeatServer interface {
-	Send(*HeartbeatsResponse) error
-	Recv() (*HeartbeatsRequest, error)
-	grpc.ServerStream
-}
-
-type polarisGRPCBatchHeartbeatServer struct {
-	grpc.ServerStream
-}
-
-func (x *polarisGRPCBatchHeartbeatServer) Send(m *HeartbeatsResponse) error {
-	return x.ServerStream.SendMsg(m)
-}
-
-func (x *polarisGRPCBatchHeartbeatServer) Recv() (*HeartbeatsRequest, error) {
-	m := new(HeartbeatsRequest)
-	if err := x.ServerStream.RecvMsg(m); err != nil {
-		return nil, err
-	}
-	return m, nil
-}
-
-func _PolarisGRPC_BatchGetHeartbeat_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(GetHeartbeatsRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(PolarisGRPCServer).BatchGetHeartbeat(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/v1.PolarisGRPC/BatchGetHeartbeat",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(PolarisGRPCServer).BatchGetHeartbeat(ctx, req.(*GetHeartbeatsRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-func _PolarisGRPC_BatchDelHeartbeat_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(DelHeartbeatsRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(PolarisGRPCServer).BatchDelHeartbeat(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/v1.PolarisGRPC/BatchDelHeartbeat",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(PolarisGRPCServer).BatchDelHeartbeat(ctx, req.(*DelHeartbeatsRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
 var _PolarisGRPC_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "v1.PolarisGRPC",
 	HandlerType: (*PolarisGRPCServer)(nil),
@@ -381,14 +258,6 @@ var _PolarisGRPC_serviceDesc = grpc.ServiceDesc{
 			MethodName: "Heartbeat",
 			Handler:    _PolarisGRPC_Heartbeat_Handler,
 		},
-		{
-			MethodName: "BatchGetHeartbeat",
-			Handler:    _PolarisGRPC_BatchGetHeartbeat_Handler,
-		},
-		{
-			MethodName: "BatchDelHeartbeat",
-			Handler:    _PolarisGRPC_BatchDelHeartbeat_Handler,
-		},
 	},
 	Streams: []grpc.StreamDesc{
 		{
@@ -397,9 +266,172 @@ var _PolarisGRPC_serviceDesc = grpc.ServiceDesc{
 			ServerStreams: true,
 			ClientStreams: true,
 		},
+	},
+	Metadata: "grpcapi.proto",
+}
+
+// PolarisHeartbeatGRPCClient is the client API for PolarisHeartbeatGRPC service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
+type PolarisHeartbeatGRPCClient interface {
+	// 被调方批量上报心跳
+	BatchHeartbeat(ctx context.Context, opts ...grpc.CallOption) (PolarisHeartbeatGRPC_BatchHeartbeatClient, error)
+	// 批量获取心跳记录
+	BatchGetHeartbeat(ctx context.Context, in *GetHeartbeatsRequest, opts ...grpc.CallOption) (*GetHeartbeatsResponse, error)
+	// 批量删除心跳记录
+	BatchDelHeartbeat(ctx context.Context, in *DelHeartbeatsRequest, opts ...grpc.CallOption) (*DelHeartbeatsResponse, error)
+}
+
+type polarisHeartbeatGRPCClient struct {
+	cc *grpc.ClientConn
+}
+
+func NewPolarisHeartbeatGRPCClient(cc *grpc.ClientConn) PolarisHeartbeatGRPCClient {
+	return &polarisHeartbeatGRPCClient{cc}
+}
+
+func (c *polarisHeartbeatGRPCClient) BatchHeartbeat(ctx context.Context, opts ...grpc.CallOption) (PolarisHeartbeatGRPC_BatchHeartbeatClient, error) {
+	stream, err := c.cc.NewStream(ctx, &_PolarisHeartbeatGRPC_serviceDesc.Streams[0], "/v1.PolarisHeartbeatGRPC/BatchHeartbeat", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &polarisHeartbeatGRPCBatchHeartbeatClient{stream}
+	return x, nil
+}
+
+type PolarisHeartbeatGRPC_BatchHeartbeatClient interface {
+	Send(*HeartbeatsRequest) error
+	Recv() (*HeartbeatsResponse, error)
+	grpc.ClientStream
+}
+
+type polarisHeartbeatGRPCBatchHeartbeatClient struct {
+	grpc.ClientStream
+}
+
+func (x *polarisHeartbeatGRPCBatchHeartbeatClient) Send(m *HeartbeatsRequest) error {
+	return x.ClientStream.SendMsg(m)
+}
+
+func (x *polarisHeartbeatGRPCBatchHeartbeatClient) Recv() (*HeartbeatsResponse, error) {
+	m := new(HeartbeatsResponse)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func (c *polarisHeartbeatGRPCClient) BatchGetHeartbeat(ctx context.Context, in *GetHeartbeatsRequest, opts ...grpc.CallOption) (*GetHeartbeatsResponse, error) {
+	out := new(GetHeartbeatsResponse)
+	err := c.cc.Invoke(ctx, "/v1.PolarisHeartbeatGRPC/BatchGetHeartbeat", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *polarisHeartbeatGRPCClient) BatchDelHeartbeat(ctx context.Context, in *DelHeartbeatsRequest, opts ...grpc.CallOption) (*DelHeartbeatsResponse, error) {
+	out := new(DelHeartbeatsResponse)
+	err := c.cc.Invoke(ctx, "/v1.PolarisHeartbeatGRPC/BatchDelHeartbeat", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// PolarisHeartbeatGRPCServer is the server API for PolarisHeartbeatGRPC service.
+type PolarisHeartbeatGRPCServer interface {
+	// 被调方批量上报心跳
+	BatchHeartbeat(PolarisHeartbeatGRPC_BatchHeartbeatServer) error
+	// 批量获取心跳记录
+	BatchGetHeartbeat(context.Context, *GetHeartbeatsRequest) (*GetHeartbeatsResponse, error)
+	// 批量删除心跳记录
+	BatchDelHeartbeat(context.Context, *DelHeartbeatsRequest) (*DelHeartbeatsResponse, error)
+}
+
+func RegisterPolarisHeartbeatGRPCServer(s *grpc.Server, srv PolarisHeartbeatGRPCServer) {
+	s.RegisterService(&_PolarisHeartbeatGRPC_serviceDesc, srv)
+}
+
+func _PolarisHeartbeatGRPC_BatchHeartbeat_Handler(srv interface{}, stream grpc.ServerStream) error {
+	return srv.(PolarisHeartbeatGRPCServer).BatchHeartbeat(&polarisHeartbeatGRPCBatchHeartbeatServer{stream})
+}
+
+type PolarisHeartbeatGRPC_BatchHeartbeatServer interface {
+	Send(*HeartbeatsResponse) error
+	Recv() (*HeartbeatsRequest, error)
+	grpc.ServerStream
+}
+
+type polarisHeartbeatGRPCBatchHeartbeatServer struct {
+	grpc.ServerStream
+}
+
+func (x *polarisHeartbeatGRPCBatchHeartbeatServer) Send(m *HeartbeatsResponse) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func (x *polarisHeartbeatGRPCBatchHeartbeatServer) Recv() (*HeartbeatsRequest, error) {
+	m := new(HeartbeatsRequest)
+	if err := x.ServerStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func _PolarisHeartbeatGRPC_BatchGetHeartbeat_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetHeartbeatsRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(PolarisHeartbeatGRPCServer).BatchGetHeartbeat(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/v1.PolarisHeartbeatGRPC/BatchGetHeartbeat",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(PolarisHeartbeatGRPCServer).BatchGetHeartbeat(ctx, req.(*GetHeartbeatsRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _PolarisHeartbeatGRPC_BatchDelHeartbeat_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(DelHeartbeatsRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(PolarisHeartbeatGRPCServer).BatchDelHeartbeat(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/v1.PolarisHeartbeatGRPC/BatchDelHeartbeat",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(PolarisHeartbeatGRPCServer).BatchDelHeartbeat(ctx, req.(*DelHeartbeatsRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+var _PolarisHeartbeatGRPC_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "v1.PolarisHeartbeatGRPC",
+	HandlerType: (*PolarisHeartbeatGRPCServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "BatchGetHeartbeat",
+			Handler:    _PolarisHeartbeatGRPC_BatchGetHeartbeat_Handler,
+		},
+		{
+			MethodName: "BatchDelHeartbeat",
+			Handler:    _PolarisHeartbeatGRPC_BatchDelHeartbeat_Handler,
+		},
+	},
+	Streams: []grpc.StreamDesc{
 		{
 			StreamName:    "BatchHeartbeat",
-			Handler:       _PolarisGRPC_BatchHeartbeat_Handler,
+			Handler:       _PolarisHeartbeatGRPC_BatchHeartbeat_Handler,
 			ServerStreams: true,
 			ClientStreams: true,
 		},
@@ -407,31 +439,31 @@ var _PolarisGRPC_serviceDesc = grpc.ServiceDesc{
 	Metadata: "grpcapi.proto",
 }
 
-func init() { proto.RegisterFile("grpcapi.proto", fileDescriptor_grpcapi_eb1402f0f1015bb1) }
+func init() { proto.RegisterFile("grpcapi.proto", fileDescriptor_grpcapi_a570c5ae8ee601f9) }
 
-var fileDescriptor_grpcapi_eb1402f0f1015bb1 = []byte{
-	// 353 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x94, 0x92, 0x3f, 0x4f, 0xf3, 0x30,
-	0x10, 0xc6, 0xdf, 0xbe, 0x48, 0x08, 0x4c, 0x5b, 0xc0, 0xfc, 0x11, 0x64, 0x64, 0x02, 0x06, 0xbb,
-	0x29, 0x03, 0x03, 0x5b, 0x1b, 0x54, 0x60, 0xaa, 0xc2, 0xc6, 0x82, 0x5c, 0xeb, 0x48, 0x2c, 0x35,
-	0xb6, 0xb1, 0xdd, 0x7c, 0x11, 0xbe, 0x23, 0x9f, 0x03, 0x25, 0x8e, 0xdb, 0xa6, 0x20, 0x21, 0x36,
-	0xdf, 0xf3, 0xdc, 0xfd, 0x2e, 0xa7, 0x3c, 0xa8, 0x97, 0x19, 0xcd, 0x99, 0x16, 0x44, 0x1b, 0xe5,
-	0x14, 0xfe, 0x5f, 0xc6, 0x51, 0x97, 0xcf, 0x05, 0x48, 0xe7, 0x95, 0xa8, 0x67, 0xc1, 0x94, 0x82,
-	0x43, 0x28, 0x0d, 0xbc, 0x2f, 0xc0, 0x06, 0xb7, 0x6f, 0xc0, 0x6a, 0x25, 0x6d, 0xb0, 0xf7, 0x73,
-	0x60, 0xc6, 0xcd, 0x80, 0x35, 0x0d, 0xc3, 0xcf, 0x2d, 0xb4, 0x37, 0x55, 0x73, 0x66, 0x84, 0x9d,
-	0xa4, 0xd3, 0x31, 0xbe, 0x46, 0xdd, 0x14, 0xb4, 0x32, 0x6e, 0x5c, 0x2f, 0xc1, 0x88, 0x94, 0x31,
-	0xf1, 0xef, 0xa8, 0x5b, 0xbd, 0xd3, 0x06, 0x78, 0xf1, 0x0f, 0x0f, 0xd0, 0x41, 0x0a, 0x99, 0xb0,
-	0x0e, 0xcc, 0xa3, 0xb4, 0x8e, 0x49, 0x0e, 0xb8, 0xee, 0x09, 0xd5, 0xb7, 0x89, 0x21, 0xc2, 0x09,
-	0x98, 0xbf, 0xcd, 0xdc, 0xa1, 0x9d, 0x44, 0x58, 0xae, 0x4a, 0x30, 0xf8, 0xa8, 0xf2, 0x42, 0x95,
-	0xfa, 0x4b, 0xa3, 0xe3, 0xb6, 0x18, 0x06, 0x2f, 0x3b, 0x83, 0x0e, 0xbe, 0x42, 0xbb, 0x0f, 0xe1,
-	0xe2, 0x5f, 0xf6, 0xdc, 0xa3, 0xfe, 0x88, 0x39, 0x9e, 0xaf, 0xfa, 0x4f, 0xaa, 0x8e, 0x65, 0x69,
-	0xc3, 0xbe, 0xd3, 0x4d, 0xb9, 0xb5, 0xf1, 0x09, 0x1d, 0xd6, 0x98, 0x09, 0xb8, 0x15, 0xe9, 0xac,
-	0x1a, 0x59, 0x57, 0x96, 0xb0, 0xf3, 0x1f, 0x9c, 0xe5, 0x27, 0x05, 0x56, 0x02, 0xf3, 0x0d, 0xd6,
-	0xba, 0xd2, 0x66, 0x6d, 0x38, 0x81, 0x35, 0xfa, 0xe8, 0xa0, 0x5b, 0xae, 0x0a, 0xe2, 0x40, 0xf2,
-	0x3a, 0x3d, 0xfe, 0xa7, 0x13, 0xab, 0x81, 0x8b, 0x37, 0xc1, 0x99, 0x13, 0x4a, 0x92, 0x2a, 0x69,
-	0x65, 0x4c, 0x42, 0xa6, 0x0a, 0x26, 0x59, 0x06, 0x23, 0xbc, 0x96, 0x90, 0x67, 0x6f, 0xbd, 0x24,
-	0x99, 0x70, 0xf9, 0x62, 0x46, 0xb8, 0x2a, 0x68, 0xc3, 0x2a, 0xc0, 0xe6, 0xb4, 0xc5, 0xa3, 0x56,
-	0x2d, 0x0c, 0x07, 0x9a, 0x29, 0xca, 0xb4, 0xa0, 0x65, 0x4c, 0x1b, 0xf2, 0xab, 0x27, 0xcf, 0xb6,
-	0xeb, 0x14, 0xde, 0x7c, 0x05, 0x00, 0x00, 0xff, 0xff, 0x14, 0xcd, 0x7b, 0x99, 0xe7, 0x02, 0x00,
-	0x00,
+var fileDescriptor_grpcapi_a570c5ae8ee601f9 = []byte{
+	// 366 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x94, 0x92, 0xbf, 0x4e, 0xeb, 0x30,
+	0x14, 0xc6, 0x6f, 0x3a, 0x5c, 0xdd, 0xeb, 0xdb, 0xf6, 0x82, 0x29, 0x08, 0x32, 0x32, 0x01, 0x83,
+	0xdd, 0x94, 0x81, 0x81, 0xad, 0x0d, 0x2a, 0x30, 0x55, 0x61, 0x63, 0x41, 0xae, 0x75, 0x48, 0x2c,
+	0x35, 0xb6, 0xb1, 0xdd, 0xbc, 0x02, 0x0f, 0xc0, 0xb3, 0xb2, 0xa3, 0xfc, 0x71, 0xda, 0x14, 0x84,
+	0xc4, 0xe6, 0x73, 0x3e, 0x7f, 0x3f, 0xfb, 0x1c, 0x7d, 0x68, 0x90, 0x1a, 0xcd, 0x99, 0x16, 0x44,
+	0x1b, 0xe5, 0x14, 0xee, 0x15, 0x51, 0xd8, 0xe7, 0x2b, 0x01, 0xd2, 0xd5, 0x9d, 0x70, 0x60, 0xc1,
+	0x14, 0x82, 0x83, 0x2f, 0x0d, 0xbc, 0xac, 0xc1, 0x7a, 0x75, 0x68, 0xc0, 0x6a, 0x25, 0xad, 0x97,
+	0xff, 0x67, 0xc0, 0x8c, 0x5b, 0x02, 0x6b, 0x2e, 0x4c, 0x5e, 0x7b, 0xe8, 0xdf, 0x42, 0xad, 0x98,
+	0x11, 0x76, 0x9e, 0x2c, 0x66, 0xf8, 0x02, 0xf5, 0x13, 0xd0, 0xca, 0xb8, 0x59, 0xf5, 0x08, 0x46,
+	0xa4, 0x88, 0x48, 0x7d, 0x0e, 0xfb, 0xe5, 0x39, 0x69, 0x80, 0xa7, 0xbf, 0xf0, 0x18, 0xed, 0x25,
+	0x90, 0x0a, 0xeb, 0xc0, 0xdc, 0x49, 0xeb, 0x98, 0xe4, 0x80, 0xab, 0x3b, 0xbe, 0xfa, 0xe4, 0x98,
+	0x20, 0x1c, 0x83, 0xf9, 0x99, 0xe7, 0x1a, 0xfd, 0x89, 0x85, 0xe5, 0xaa, 0x00, 0x83, 0x0f, 0x4a,
+	0xcd, 0x57, 0x49, 0x3d, 0x69, 0x38, 0xea, 0x36, 0xbd, 0xf1, 0x2c, 0x18, 0x07, 0xf8, 0x1c, 0xfd,
+	0xbd, 0xf5, 0x13, 0x7f, 0xff, 0xce, 0xe4, 0x3d, 0x40, 0xa3, 0x66, 0x13, 0xad, 0xa5, 0x5a, 0xc9,
+	0x0d, 0x1a, 0x4e, 0x99, 0xe3, 0xd9, 0x06, 0x74, 0x58, 0x5a, 0xdb, 0xd2, 0xfa, 0x8f, 0x1c, 0xed,
+	0xb6, 0x3b, 0x5f, 0xb9, 0x47, 0xfb, 0x15, 0x66, 0x0e, 0x6e, 0x43, 0x3a, 0x2e, 0x2d, 0xdb, 0x9d,
+	0x16, 0x76, 0xf2, 0x85, 0xd2, 0xee, 0xc4, 0xb3, 0x62, 0x58, 0xed, 0xb0, 0xb6, 0x3b, 0x5d, 0xd6,
+	0x8e, 0xe2, 0x59, 0xd3, 0xb7, 0x00, 0x5d, 0x71, 0x95, 0x13, 0x07, 0x92, 0x57, 0xb1, 0xaa, 0x77,
+	0x40, 0xac, 0x06, 0x2e, 0x9e, 0x05, 0x67, 0x4e, 0x28, 0x49, 0xca, 0x08, 0x16, 0x11, 0xf1, 0x61,
+	0xcb, 0x99, 0x64, 0x29, 0x4c, 0xf1, 0x56, 0x74, 0x1e, 0x6a, 0xe9, 0x31, 0x4e, 0x85, 0xcb, 0xd6,
+	0x4b, 0xc2, 0x55, 0x4e, 0x1b, 0x56, 0x0e, 0x36, 0xa3, 0x1d, 0x1e, 0xb5, 0x6a, 0x6d, 0x38, 0xd0,
+	0x54, 0x51, 0xa6, 0x05, 0x2d, 0x22, 0xda, 0x90, 0x9f, 0x6a, 0xf2, 0xf2, 0x77, 0x15, 0xcf, 0xcb,
+	0x8f, 0x00, 0x00, 0x00, 0xff, 0xff, 0x9b, 0x92, 0xc7, 0xf5, 0x00, 0x03, 0x00, 0x00,
 }
