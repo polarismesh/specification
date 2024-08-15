8c58ad52ac022eb5ef2e57c353d5cb2aa87cec5f
andrew shan <45474304+andrewshan@users.noreply.github.com>
2024-08-15 14:51:25 +0800

    feat: restore circuitbreaker fallback & ratelimit add custom response

--- source/java/build-mac.sh
+++ source/java/build-mac.sh
@@ -26,7 +26,6 @@ traffic_manage_dir=${workdir}/api/v1/traffic_manage
 fault_tolerance_dir=${workdir}/api/v1/fault_tolerance
 config_manage_dir=${workdir}/api/v1/config_manage
 security_dir=${workdir}/api/v1/security
-
 java_root_dir=${workdir}/source/java/polaris-specification
 java_test_root_dir=${workdir}/source/java/polaris-specification-test
 java_source_dir=${java_root_dir}/src/main
@@ -45,8 +44,17 @@ cp ${traffic_manage_dir}/ratelimiter/*.proto ${java_source_dir}/proto/
 version=`cat ${workdir}/VERSION`
 echo $version
 pushd ${java_root_dir}
+cp pom.xml pom.xml.bak
 sed -i "" "s/##VERSION##/${version}/g" pom.xml
+mvn clean install
+mv pom.xml.bak pom.xml
 popd
 pushd ${java_test_root_dir}
+cp pom.xml pom.xml.bak
 sed -i "" "s/##VERSION##/${version}/g" pom.xml
+mvn clean install
+mv pom.xml.bak pom.xml
 popd
+
+rm -rf ${java_source_dir}/proto
+
